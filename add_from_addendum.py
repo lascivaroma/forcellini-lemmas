2fb70045fdb6c9080c1b77623d527b2d9bd52e29
Thibault Clérice <leponteineptique@gmail.com>
2020-09-01 09:51:02 +0200

    Added headwords from Anthony Glaise's work

--- add_from_addendum.py
+++ add_from_addendum.py
@@ -8,32 +8,32 @@ import csv
 
 lemmas = {}
 fieldnames = []
+HEADER = "lemma	Nature	POS	Description	Variation	Code".split("\t")
 
-with open("dictionnary.tsv") as f:
+with open("dictionary.tsv") as f:
 	reader = csv.DictReader(f, delimiter="\t")
 	for line in reader:
 		fieldnames = reader.fieldnames
-		if (line["Lemma"], line["Nature"]) not in lemmas:
-			lemmas[(line["Lemma"], line["Nature"])] = line
+		if (line["lemma"], line["Nature"]) not in lemmas:
+			lemmas[(line["lemma"], line["Nature"])] = line
 
 print(fieldnames)
 before = len(lemmas)
 print("{} actual lemmata.".format(len(lemmas)))
 
-for file in glob.glob("addendum/*.tsv"):
+for file in glob.glob("addendum/latin-tardif.tsv"):
 	print(file)
 	with open(file) as f:
 		reader = csv.DictReader(f, delimiter="\t")
 		for line in reader:
-			print(line)
-			if (line["Lemma"], line["Nature"]) not in lemmas:
-				lemmas[(line["Lemma"], line["Nature"])] = line
+			line["lemma"] = line["lemma"].replace("v", "u").replace("V", "U").replace("J", "I").replace("j", "i")
+			if (line["lemma"], line["Nature"]) not in lemmas:
+				lemmas[(line["lemma"], line["Nature"])] = line
 
 print("{} new lemmata.".format(len(lemmas) - before))
 
-with open("newdictionnary.tsv", "w") as f:
-	writer = csv.DictWriter(f, fieldnames, extrasaction="ignore", delimiter="\t")
-	f.write("lemma	Nature	POS	Code	Description	Variations\n")
-	#writer.writeheader()
+with open("dictionary.tsv", "w") as f:
+	writer = csv.DictWriter(f, HEADER, extrasaction="ignore", delimiter="\t", lineterminator="\n")
+	writer.writeheader()
 	for lemma in sorted(list(lemmas.keys()), key=lambda x: x[0].lower()+x[1]):
 		writer.writerow(lemmas[lemma])
